675641df2154c088936d9d131a4875121da13044
Fhem <fhem24_elle@yahoo.com>
2015-08-17 10:54:08 +0800

    Prevent redownload if STS already exists

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,22 @@
-
 class springtoolsuite {
 	
 	$tar_filename = 'spring-tool-suite-3.7.0.RELEASE-e4.5-macosx-cocoa-x86_64.tar.gz'
 
 	exec {"curl_springtoolsuite":
             command => "/usr/bin/curl -O http://dist.springsource.com/release/STS/3.7.0.RELEASE/dist/e4.5/$tar_filename",
-            unless  => "test -f /tmp/${tar_filename} -o -d /Applications/springsource",
-            cwd     => "/tmp",
+            unless  => "test -f /private/tmp/${tar_filename}",
+            cwd     => "/private/tmp",
             timeout => 0
     }
 
 	exec {"extract_springtoolsuite":
-            command => "/usr/bin/tar -xzf /tmp/$tar_filename",
+            command => "/usr/bin/tar -xzf /private/tmp/$tar_filename",
             unless  => "test -d /Applications/springsource",
             cwd     => "/Applications",
             require => Exec["curl_springtoolsuite"],	
     }
 
-	file {"/tmp/$tar_filename":
+	file {"/private/tmp/$tar_filename":
 		ensure => absent,
 		require => Exec["extract_springtoolsuite"]
 	}
